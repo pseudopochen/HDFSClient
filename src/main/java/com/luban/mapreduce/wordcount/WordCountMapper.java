8bb2405bc7f64d37b7f2805ac3e07c59dc129915
pseudopochen <pseudopochen@gmail.com>
2022-04-21 15:55:11 -0600

    mr word count

---
+++ src/main/java/com/luban/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,24 @@
+package com.luban.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text outK = new Text();
+    private IntWritable outV = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            outK.set(word);
+            context.write(outK, outV);
+        }
+    }
+}
